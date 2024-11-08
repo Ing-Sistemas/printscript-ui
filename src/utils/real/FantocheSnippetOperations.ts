d11747665cc3c4a46017b0876e89a019f1ad3f16
= <aalma@mail.austral.edu.ar>
2024-11-08 17:15:31 -0300

    fixed deleteSnippet and conversion methods

--- src/utils/real/FantocheSnippetOperations.ts
+++ src/utils/real/FantocheSnippetOperations.ts
@@ -13,6 +13,37 @@ export class FantocheSnippetOperations implements SnippetOperations {
 
     private token = localStorage.getItem("token");
 
+    async createSnippet(createSnippet: CreateSnippet): Promise<Snippet> {
+        try {
+            const url = `${BACKEND_URL}/create`;
+            const data = transformSnippet(createSnippet);
+            const res = await axios.post(url, data, {
+                headers: {
+                    'Authorization': `Bearer ${this.token}`,
+                },
+            });
+            return res.data.snippetEntity; // TODO ver si esto esta bien
+        } catch (e) {
+            console.log("Failed to create snippet", e);
+            throw e;
+        }
+    }
+
+    async deleteSnippet(id: string): Promise<string> {
+        try {
+            const url = `${BACKEND_URL}/delete/${id}`;
+            await axios.delete(url, {
+                headers: {
+                    'Authorization': `Bearer ${this.token}`,
+                },
+            });
+            return "Snippet deleted successfully";
+        } catch (e) {
+            console.log("Error deleting snippet", e);
+            throw e;
+        }
+    }
+
     async getSnippetById(id: string): Promise<Snippet | undefined> {
         // TODO arreglar el return para q lo q se reciba sea acorde a lo q pide el Snippet type
         try {
@@ -51,13 +82,47 @@ export class FantocheSnippetOperations implements SnippetOperations {
             throw e;
         }
     }
-    getUserFriends(name?: string | undefined, page?: number | undefined, pageSize?: number | undefined): Promise<PaginatedUsers> {
-        throw new Error("Method not implemented.");
-    }
     shareSnippet(snippetId: string, userId: string): Promise<Snippet> {
         throw new Error("Method not implemented.");
     }
+    listSnippetDescriptors(page: number, pageSize: number, sippetName?: string | undefined): Promise<PaginatedSnippets> {
+        throw new Error("Method not implemented.");
+    }
+
+    getUserFriends(name?: string | undefined, page?: number | undefined, pageSize?: number | undefined): Promise<PaginatedUsers> {
+        throw new Error("Method not implemented.");
+    }
+
+    // ------------------- TEST CASES -------------------
+    getTestCases(snippetId: string): Promise<TestCase[]> {
+        throw new Error("Method not implemented.");
+    }
+    postTestCase(testCase: Partial<TestCase>): Promise<TestCase> {
+        throw new Error("Method not implemented.");
+    }
+    removeTestCase(id: string): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
+    testSnippet(testCase: Partial<TestCase>): Promise<TestCaseResult> {
+        throw new Error("Method not implemented.");
+    }
+    getFileTypes(): Promise<FileType[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    // ------------------- FORMAT CASES -------------------
     getFormatRules(): Promise<Rule[]> {
+        throw new Error("Method not implemented.");
+    }
+    modifyFormatRule(newRules: Rule[]): Promise<Rule[]> {
+        throw new Error("Method not implemented.");
+    }
+    formatSnippet(snippet: string): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
+
+    // ------------------- LINT CASES -------------------
+    modifyLintingRule(newRules: Rule[]): Promise<Rule[]> {
         throw new Error("Method not implemented.");
     }
     async getLintingRules(): Promise<Rule[]> {
@@ -74,62 +139,5 @@ export class FantocheSnippetOperations implements SnippetOperations {
             throw e;
         }
     }
-    getTestCases(snippetId: string): Promise<TestCase[]> {
-        throw new Error("Method not implemented.");
-    }
-    formatSnippet(snippet: string): Promise<string> {
-        throw new Error("Method not implemented.");
-    }
-    postTestCase(testCase: Partial<TestCase>): Promise<TestCase> {
-        throw new Error("Method not implemented.");
-    }
-    removeTestCase(id: string): Promise<string> {
-        throw new Error("Method not implemented.");
-    }
-    testSnippet(testCase: Partial<TestCase>): Promise<TestCaseResult> {
-        throw new Error("Method not implemented.");
-    }
-    getFileTypes(): Promise<FileType[]> {
-        throw new Error("Method not implemented.");
-    }
-    modifyFormatRule(newRules: Rule[]): Promise<Rule[]> {
-        throw new Error("Method not implemented.");
-    }
-    modifyLintingRule(newRules: Rule[]): Promise<Rule[]> {
-        throw new Error("Method not implemented.");
-    }
-    listSnippetDescriptors(page: number, pageSize: number, sippetName?: string | undefined): Promise<PaginatedSnippets> {
-        throw new Error("Method not implemented.");
-    }
 
-    async deleteSnippet(id: string): Promise<string> {
-        try {
-            const url = `${BACKEND_URL}/delete/${id}`; //TODO add snippetId to s-s url
-            await axios.delete(url, {
-                headers: {
-                    'Authorization': `Bearer ${this.token}`,
-                },
-            });
-            return "Snippet deleted successfully";
-        } catch (e) {
-            console.log("Error deleting snippet", e);
-            throw e;
-        }
-    }
-
-    async createSnippet(createSnippet: CreateSnippet): Promise<Snippet> {
-        try {
-            const url = `${BACKEND_URL}/create`;
-            const data = transformSnippet(createSnippet);
-            const res = await axios.post(url, data, {
-                headers: {
-                    'Authorization': `Bearer ${this.token}`,
-                },
-            });
-            return res.data;
-        } catch (e) {
-            console.log("Failed to create snippet", e);
-            throw e;
-        }
-    }
 }
