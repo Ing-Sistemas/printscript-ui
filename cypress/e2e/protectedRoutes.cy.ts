555333c2c806e597148ac19cff908d46008b428a
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-27 12:34:42 -0300

    changed things

--- cypress/e2e/protectedRoutes.cy.ts
+++ cypress/e2e/protectedRoutes.cy.ts
@@ -6,24 +6,23 @@ describe('Protected routes test', () => {
       return false
     })
   })
-
   it('should redirect to login when accessing a protected route unauthenticated', () => {
 
     // Visit the protected route
-    cy.visit('/localhost:5173');
+    cy.visit("/");
 
     cy.wait(1000)
-
+    console.log(cy.url())
     // Check if the URL is redirected to the login page
     cy.url().should('include', '/login');
   });
 
   it('should display login content', () => {
     // Visit the login page
-    cy.visit('https://dev-s0u3fhv7614agdfp.us.auth0.com');
+    cy.visit('/login');
 
     // Look for text that is likely to appear on a login page
-    //cy.contains('Email address').should('exist'); no se pq no anda
+    cy.contains('Email address').should('exist');
     cy.contains('Password').should('exist'); // Adjust the text based on actual content
   });
 
